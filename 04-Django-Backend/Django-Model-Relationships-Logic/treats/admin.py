d3453aeabe49ed7163f450ef7187a01a959d91e9
BuiltByShyryn <nika.kika.1996@mail.ru>
2026-02-15 23:00:25 +0500

    Multi-Entity Relationship Mapping
    A Django project demonstrating complex relational mapping between four distinct entities. I implemented 'Parent-Child' hierarchy logic and 'Vendor-Product' links using Django ForeignKeys. This project focuses on data validation, ensuring no empty fields reach the database, and custom Admin Panel registration for data management.

---
+++ 04-Django-Backend/Django-Model-Relationships-Logic/treats/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Human, Child, Ice_Cream, Kiosk
+
+admin.site.register(Human)
+admin.site.register(Child)
+admin.site.register(Ice_Cream)
+admin.site.register(Kiosk)
---
+++ 04-Django-Backend/Django-Model-Relationships-Logic/treats/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+#Человек, Ребёнок, Мороженное, Киоск с мороженным
+# Create your models here.
+
+class Human(models.Model):
+    name = models.CharField(max_length=255,blank=False, null=False)
+    age = models.IntegerField()
+    
+    def __str__(self):
+        return self.name 
+        
+class Child(models.Model): 
+    name = models.CharField(max_length=255,blank=False, null=False)
+    age = models.IntegerField(blank=False, null=False)
+    parent = models.ForeignKey(Human,on_delete=models.CASCADE)
+   
+    def __str__(self):
+        return self.name 
+    
+class Ice_Cream(models.Model):
+    name = models.CharField(max_length=255,blank=False, null=False)
+    desc = models.TextField(max_length=1000)
+    def __str__(self):
+        return self.name 
+class Kiosk(models.Model):
+    name = models.CharField(max_length=255,blank=False, null=False)
+    ice_cream_type = models.ForeignKey(Ice_Cream, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name 
