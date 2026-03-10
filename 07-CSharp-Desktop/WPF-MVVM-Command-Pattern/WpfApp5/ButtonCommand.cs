fdd433e910baf3df048092271b88a13faeaf36b4
BuiltByShyryn <nika.kika.1996@mail.ru>
2026-03-10 22:53:58 +0500

    feat: implement custom ButtonCommand for MVVM pattern in WPF
    Custom implementation of the ICommand interface for WPF, demonstrating advanced event handling with CommandManager and decoupled UI logic.

---
+++ 07-CSharp-Desktop/WPF-MVVM-Command-Pattern/WpfApp5/ButtonCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfApp5
+{
+    internal class ButtonCommand : ICommand
+    {
+        //public event EventHandler CanExecuteChanged;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        Action<object> action;
+        Func<object, bool> canExecute;
+        public ButtonCommand(Action<object> action, Func <object,bool> canExecute)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute!= null || canExecute(parameter); ; 
+        }
+
+        public void Execute(object parameter)
+        {
+            action(parameter);
+        }
+    }
+}
---
+++ 07-CSharp-Desktop/WPF-MVVM-Command-Pattern/WpfApp5/ViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp5
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        int count = 0;
+
+        public int Count
+        {
+            get { return count;
+            }
+        set
+            {
+                count = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Count"));
+            }
+        
+        }
+
+
+        public ButtonCommand Click
+        {
+            get
+            {
+                return new ButtonCommand((param) =>
+                {
+                    Count += Convert.ToInt32(param.ToString());
+                }, (param) =>
+                {
+                    return
+                    param.ToString()[0]=='1' ||
+                    Convert.ToInt32(param.ToString()) * 10
+                    <= count;
+                }
+                
+                
+                );
+            }
+        }
+    }
+}
