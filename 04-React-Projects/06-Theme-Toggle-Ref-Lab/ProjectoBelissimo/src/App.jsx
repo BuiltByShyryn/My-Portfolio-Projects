b01d8efd176759a536bf3f01e6ec4a87735dd2cf
BuiltByShyryn <nika.kika.1996@mail.ru>
2026-03-11 00:06:58 +0500

    Refactor: fix imports, case sensitivity, and hook implementation

--- 04-React-Projects/06-Theme-Toggle-Ref-Lab/ProjectoBelissimo/src/App.jsx
+++ 04-React-Projects/06-Theme-Toggle-Ref-Lab/ProjectoBelissimo/src/App.jsx
@@ -2,19 +2,24 @@ import { useState } from 'react'
 import Form from "./Form"
 import './App.css'
 import Switch from './Switch'
-
+import { useState, useRef } from 'react'
 
 function App() {
   const[value,setValue] = useState("")
-const bodyInput = useRef();
+  const bodyInput = useRef();
 
 console.log(bodyInput.current.value)
 
   return (
     <>
-    <input type="text" ref={bodyInput} value ={value} onChange={(E)=> setValue(e.target.value)} />
-<Form/>
-<Switch/>
+    <input 
+        type="text" 
+        ref={bodyInput} 
+        value={value} 
+        onChange={(e) => setValue(e.target.value)} 
+      />
+    <Form/>
+    <Switch/>
 
     </>
   )
