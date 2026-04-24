9f756ec8374670b0cef84eb89dafe67a3c8289fd
BuiltByShyryn <nika.kika.1996@mail.ru>
2026-04-24 19:35:50 +0500

    feat:  Lets the user choose an executable file , Launches a process with optional command-line arguments , Uses a Windows Forms interface
    A Windows Forms utility for selecting and starting external programs with custom arguments.

---
+++ 07-CSharp-Desktop/20-WPF_ProcessLauncher/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace RunningProc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtProgName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txtArgs = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtProgName
+            // 
+            this.txtProgName.Location = new System.Drawing.Point(145, 50);
+            this.txtProgName.Name = "txtProgName";
+            this.txtProgName.Size = new System.Drawing.Size(351, 20);
+            this.txtProgName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Programmas:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Arguments:";
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(573, 50);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 4;
+            this.btnFind.Text = "Search";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(421, 149);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(291, 149);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 6;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // txtArgs
+            // 
+            this.txtArgs.Location = new System.Drawing.Point(145, 85);
+            this.txtArgs.Name = "txtArgs";
+            this.txtArgs.Size = new System.Drawing.Size(351, 20);
+            this.txtArgs.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtArgs);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtProgName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtProgName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtArgs;
+    }
+}
+
---
+++ 07-CSharp-Desktop/20-WPF_ProcessLauncher/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace RunningProc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Exe - files(*.*) | *.exe|*.*|All files(*.*)";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtProgName.Text = ofd.FileName;
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                // Process.Start(txtProgName.Text,txtArgs.Text);
+
+                startInfo.FileName = txtProgName.Text;
+                startInfo.Arguments = txtArgs.Text;
+                Process.Start(startInfo);
+            }
+            catch (Exception ex){
+               MessageBox.Show(ex.Message,"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
