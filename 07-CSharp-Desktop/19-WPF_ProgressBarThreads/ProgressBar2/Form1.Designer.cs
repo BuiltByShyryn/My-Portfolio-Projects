54561d5d1943918e086ef084f372fcf241392392
BuiltByShyryn <nika.kika.1996@mail.ru>
2026-04-24 19:34:18 +0500

    Uses a Windows Forms interface with several progress bars, Demonstrates timer-based progress updates, Demonstrates thread-based progress bar work, Includes start, pause, and resume controls
    A Windows Forms project that shows how timers and threads can be used to update progress bars in different ways.

---
+++ 07-CSharp-Desktop/19-WPF_ProgressBarThreads/ProgressBar2/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace ProgressBar2
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.prbTimer = new System.Windows.Forms.ProgressBar();
+            this.prbThread1 = new System.Windows.Forms.ProgressBar();
+            this.prbThread2 = new System.Windows.Forms.ProgressBar();
+            this.btnStartTimer = new System.Windows.Forms.Button();
+            this.btnPauseTimer = new System.Windows.Forms.Button();
+            this.btnStart1 = new System.Windows.Forms.Button();
+            this.btnPause1 = new System.Windows.Forms.Button();
+            this.btnStart2 = new System.Windows.Forms.Button();
+            this.btnPause2 = new System.Windows.Forms.Button();
+            this.btnPauseAll = new System.Windows.Forms.Button();
+            this.btnResumeAll = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtPeriodTimer = new System.Windows.Forms.TextBox();
+            this.txtPeriod1 = new System.Windows.Forms.TextBox();
+            this.txtPeriod2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // prbTimer
+            // 
+            this.prbTimer.Location = new System.Drawing.Point(66, 58);
+            this.prbTimer.Name = "prbTimer";
+            this.prbTimer.Size = new System.Drawing.Size(537, 52);
+            this.prbTimer.TabIndex = 0;
+            // 
+            // prbThread1
+            // 
+            this.prbThread1.Location = new System.Drawing.Point(66, 139);
+            this.prbThread1.Name = "prbThread1";
+            this.prbThread1.Size = new System.Drawing.Size(537, 52);
+            this.prbThread1.TabIndex = 1;
+            // 
+            // prbThread2
+            // 
+            this.prbThread2.Location = new System.Drawing.Point(66, 220);
+            this.prbThread2.Name = "prbThread2";
+            this.prbThread2.Size = new System.Drawing.Size(537, 52);
+            this.prbThread2.TabIndex = 2;
+            // 
+            // btnStartTimer
+            // 
+            this.btnStartTimer.Location = new System.Drawing.Point(619, 58);
+            this.btnStartTimer.Name = "btnStartTimer";
+            this.btnStartTimer.Size = new System.Drawing.Size(75, 52);
+            this.btnStartTimer.TabIndex = 3;
+            this.btnStartTimer.Text = "Start";
+            this.btnStartTimer.UseVisualStyleBackColor = true;
+            this.btnStartTimer.Click += new System.EventHandler(this.btnStartTimer_Click);
+            // 
+            // btnPauseTimer
+            // 
+            this.btnPauseTimer.Location = new System.Drawing.Point(700, 58);
+            this.btnPauseTimer.Name = "btnPauseTimer";
+            this.btnPauseTimer.Size = new System.Drawing.Size(75, 52);
+            this.btnPauseTimer.TabIndex = 4;
+            this.btnPauseTimer.Text = "Pause";
+            this.btnPauseTimer.UseVisualStyleBackColor = true;
+            this.btnPauseTimer.Click += new System.EventHandler(this.btnPauseTimer_Click);
+            // 
+            // btnStart1
+            // 
+            this.btnStart1.Location = new System.Drawing.Point(619, 139);
+            this.btnStart1.Name = "btnStart1";
+            this.btnStart1.Size = new System.Drawing.Size(75, 52);
+            this.btnStart1.TabIndex = 5;
+            this.btnStart1.Tag = "Button-1";
+            this.btnStart1.Text = "Start";
+            this.btnStart1.UseVisualStyleBackColor = true;
+            this.btnStart1.Click += new System.EventHandler(this.btnStart1_Click);
+            // 
+            // btnPause1
+            // 
+            this.btnPause1.Location = new System.Drawing.Point(700, 139);
+            this.btnPause1.Name = "btnPause1";
+            this.btnPause1.Size = new System.Drawing.Size(75, 52);
+            this.btnPause1.TabIndex = 6;
+            this.btnPause1.Text = "Pause";
+            this.btnPause1.UseVisualStyleBackColor = true;
+            // 
+            // btnStart2
+            // 
+            this.btnStart2.Location = new System.Drawing.Point(619, 220);
+            this.btnStart2.Name = "btnStart2";
+            this.btnStart2.Size = new System.Drawing.Size(75, 52);
+            this.btnStart2.TabIndex = 7;
+            this.btnStart2.Tag = "Button-2";
+            this.btnStart2.Text = "Start";
+            this.btnStart2.UseVisualStyleBackColor = true;
+            this.btnStart2.Click += new System.EventHandler(this.btnStart1_Click);
+            // 
+            // btnPause2
+            // 
+            this.btnPause2.Location = new System.Drawing.Point(700, 220);
+            this.btnPause2.Name = "btnPause2";
+            this.btnPause2.Size = new System.Drawing.Size(75, 52);
+            this.btnPause2.TabIndex = 8;
+            this.btnPause2.Text = "Pause";
+            this.btnPause2.UseVisualStyleBackColor = true;
+            // 
+            // btnPauseAll
+            // 
+            this.btnPauseAll.Location = new System.Drawing.Point(187, 329);
+            this.btnPauseAll.Name = "btnPauseAll";
+            this.btnPauseAll.Size = new System.Drawing.Size(121, 23);
+            this.btnPauseAll.TabIndex = 9;
+            this.btnPauseAll.Text = "Pause for all";
+            this.btnPauseAll.UseVisualStyleBackColor = true;
+            this.btnPauseAll.Click += new System.EventHandler(this.btnPauseAll_Click);
+            // 
+            // btnResumeAll
+            // 
+            this.btnResumeAll.Location = new System.Drawing.Point(378, 329);
+            this.btnResumeAll.Name = "btnResumeAll";
+            this.btnResumeAll.Size = new System.Drawing.Size(124, 23);
+            this.btnResumeAll.TabIndex = 10;
+            this.btnResumeAll.Text = "Resume for all";
+            this.btnResumeAll.UseVisualStyleBackColor = true;
+            this.btnResumeAll.Click += new System.EventHandler(this.btnResumeAll_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(558, 329);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(110, 23);
+            this.btnClose.TabIndex = 11;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // txtPeriodTimer
+            // 
+            this.txtPeriodTimer.Location = new System.Drawing.Point(788, 75);
+            this.txtPeriodTimer.Name = "txtPeriodTimer";
+            this.txtPeriodTimer.Size = new System.Drawing.Size(100, 20);
+            this.txtPeriodTimer.TabIndex = 12;
+            // 
+            // txtPeriod1
+            // 
+            this.txtPeriod1.Location = new System.Drawing.Point(788, 156);
+            this.txtPeriod1.Name = "txtPeriod1";
+            this.txtPeriod1.Size = new System.Drawing.Size(100, 20);
+            this.txtPeriod1.TabIndex = 13;
+            // 
+            // txtPeriod2
+            // 
+            this.txtPeriod2.Location = new System.Drawing.Point(788, 237);
+            this.txtPeriod2.Name = "txtPeriod2";
+            this.txtPeriod2.Size = new System.Drawing.Size(100, 20);
+            this.txtPeriod2.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(797, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Time of period";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 402);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPeriod2);
+            this.Controls.Add(this.txtPeriod1);
+            this.Controls.Add(this.txtPeriodTimer);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnResumeAll);
+            this.Controls.Add(this.btnPauseAll);
+            this.Controls.Add(this.btnPause2);
+            this.Controls.Add(this.btnStart2);
+            this.Controls.Add(this.btnPause1);
+            this.Controls.Add(this.btnStart1);
+            this.Controls.Add(this.btnPauseTimer);
+            this.Controls.Add(this.btnStartTimer);
+            this.Controls.Add(this.prbThread2);
+            this.Controls.Add(this.prbThread1);
+            this.Controls.Add(this.prbTimer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ProgressBar prbTimer;
+    private System.Windows.Forms.ProgressBar prbThread1;
+    private System.Windows.Forms.ProgressBar prbThread2;
+    private System.Windows.Forms.Button btnStartTimer;
+    private System.Windows.Forms.Button btnPauseTimer;
+    private System.Windows.Forms.Button btnStart1;
+    private System.Windows.Forms.Button btnPause1;
+    private System.Windows.Forms.Button btnStart2;
+    private System.Windows.Forms.Button btnPause2;
+    private System.Windows.Forms.Button btnPauseAll;
+    private System.Windows.Forms.Button btnResumeAll;
+    private System.Windows.Forms.Button btnClose;
+    private System.Windows.Forms.TextBox txtPeriodTimer;
+    private System.Windows.Forms.TextBox txtPeriod1;
+    private System.Windows.Forms.TextBox txtPeriod2;
+    private System.Windows.Forms.Label label1;
+  }
+}
+
---
+++ 07-CSharp-Desktop/19-WPF_ProgressBarThreads/ProgressBar2/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics.Eventing.Reader;
+
+
+namespace ProgressBar2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtPeriodTimer.Text = "0,1";
+            txtPeriod1.Text = "0,1";
+            txtPeriod2.Text = "0,1";
+
+            tmrProgressBar = new System.Windows.Forms.Timer();
+            tmrProgressBar.Tick += new EventHandler(TimerRoutine); // ф-ция обработчик события таймера
+            tmrProgressBar.Stop();
+        }
+        private System.Windows.Forms.Timer tmrProgressBar = null;  // new Timer();
+        private Thread th1 = null, th2 = null;
+        private Mutex stopMutex = new Mutex(false);
+        private void TimerRoutine(object sender, EventArgs e)
+        {
+            tmrProgressBar.Stop();
+            this.Invoke(new Action(() =>
+            {
+                prbTimer.PerformStep();  // prbTimer.value += prbTimer.Step;
+            }));
+            tmrProgressBar.Start();
+        }
+
+        private void btnStartTimer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int interval = (int)(float.Parse(txtPeriodTimer.Text) * 1000);
+                tmrProgressBar.Interval = interval; // для таймера интервал
+                                                    // настройки прогресс-бара
+                prbTimer.Minimum = 1;
+                prbTimer.Maximum = 500; // 1000
+                prbTimer.Value = prbTimer.Minimum;
+                prbTimer.Step = 1;
+                //
+                tmrProgressBar.Start(); // запук таймера
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error",
+                                 MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        } // void btnStartTimer_Click(object sender, EventArgs e)
+
+        private void btnPauseTimer_Click(object sender, EventArgs e)
+        {
+            tmrProgressBar.Stop();
+        }
+        
+        private void btnStart1_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            string btn_whois = (string)btn.Tag;
+            Thread th_temp;
+            ProgressBar prb_temp;
+           
+            if (btn_whois == "Button-1")
+            {
+                th_temp = th1;
+                prb_temp = prbThread1;
+            }
+            else
+            {
+                th_temp = th2;
+                prb_temp = prbThread2;
+            }
+            if (th_temp == null)
+            {
+                th_temp = new Thread(ThreadProc_PB);
+                th_temp.IsBackground = true;
+                th_temp.Start(prb_temp);
+            }
+            if (btn_whois == "Button-1") th1 = th_temp;
+            else th2 = th_temp;
+        }
+
+        private void btnPauseAll_Click(object sender, EventArgs e)
+        {
+            stopMutex.WaitOne();
+        }
+
+        private void btnResumeAll_Click(object sender, EventArgs e)
+        {
+            stopMutex.ReleaseMutex();
+        }
+
+        private void ThreadProc_PB(object param)
+        {
+// ProgressBar prb = param as ProgressBar;
+            ProgressBar prb = (ProgressBar)param;
+            int time_out = 200;
+            prb.Value = prb.Minimum;
+            while (true)
+            {
+                prb.PerformStep();
+                prb.Invalidate(true);
+                prb.Update();
+
+                if (prb.Value == prb.Maximum)
+                {
+                    break;
+                }
+                Thread.Sleep(time_out);
+
+            }
+            if (this.prbThread1 == prb)
+            {
+                th1 = null;
+            }
+            else
+            {
+                th2 = null;
+            }
+
+        }
+
+
+
+    }
+}
