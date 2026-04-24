76f18c1a1c9aad1961a01ca9ca38368776bc7657
BuiltByShyryn <nika.kika.1996@mail.ru>
2026-04-24 19:36:51 +0500

    feat:  Uses a Windows Forms interface with several progress bars, Demonstrates timer-based progress updates ,Demonstrates thread-based progress bar work - Includes start, pause, and resume controls
    A Windows Forms project that shows how timers and threads can be used to update progress bars in different ways.

---
+++ 07-CSharp-Desktop/21-WPF_InterlockedCounterSample/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace InterlockedSample
+{
+    internal class Program
+    {
+
+        static int Summa = 0;
+        static Semaphore work_sem = null;
+        static Stopwatch work_time = null;
+        static long TotalTime = 0;
+
+        static void Main(string[] args)
+        {
+
+            int thread_count = 10;
+            int count = 10000;
+
+            
+            ThreadPool.SetMinThreads(thread_count, thread_count);
+            work_sem = new Semaphore(thread_count, thread_count);
+
+            for (int i = 0; i < thread_count; i++)
+            {
+                work_sem.WaitOne();
+                ThreadPool.QueueUserWorkItem(ThreadProc, count);
+            }
+            //Thread.Sleep(2000);
+
+            for (int i = 0; i<thread_count; i++)
+            {
+                work_sem.WaitOne();
+            }
+            Console.WriteLine($"Summa: {Summa}");
+            Console.ReadLine();
+        }
+
+        static void ThreadProc(object param)
+        {
+
+            int count = (int)param;
+            int id = Thread.CurrentThread.ManagedThreadId;
+            
+            for (int i = 0; i < count; i++)
+            {
+                Summa++;
+            }
+            Console.WriteLine($"id:{id} started to work");
+            
+            Stopwatch work_time = new Stopwatch();
+            work_time.Reset();
+            
+
+            for (int i = 0;i < count; i++)
+            {
+                Interlocked.Add(ref Summa, 1);
+            }
+            work_time.Stop();
+            Interlocked.Add(ref TotalTime, work_time.ElapsedTicks);
+            Console.WriteLine($"The time:{TotalTime /10.0} microsec");
+            work_sem.Release();
+            
+        }
+    }
+}
