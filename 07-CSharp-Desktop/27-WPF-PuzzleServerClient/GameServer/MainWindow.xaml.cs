69424cb637b8f1ed5b1c09ffdf69f2d3dadeb65a
BuiltByShyryn <nika.kika.1996@mail.ru>
2026-05-04 21:55:48 +0500

    patch: password safety was added.

--- 07-CSharp-Desktop/27-WPF-PuzzleServerClient/GameServer/MainWindow.xaml.cs
+++ 07-CSharp-Desktop/27-WPF-PuzzleServerClient/GameServer/MainWindow.xaml.cs
@@ -565,7 +565,12 @@ namespace GameServer
 
         private string MakeHash(string password)
         {
-            return password;
+            using (var sha = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(password);
+                byte[] hash = sha.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
+            }
         }
 
         private void btnSaveLog_Click(object sender, RoutedEventArgs e)
